789eb8a6a395ef350f8772f252d80fd1c9b51ae3
mohdadil27 <mohdadil2721@gmail.com>
2022-07-27 14:56:06 +0530

    Updates

---
+++ precache-manifest.54acb517b49c19835abab05fa6433f37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8a74299d592e8f9c321af54f03896a",
+    "url": "/Hotelmenu-react-003/index.html"
+  },
+  {
+    "revision": "1d4c0c51aec40d05301c",
+    "url": "/Hotelmenu-react-003/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "4642341b78d581a3b872",
+    "url": "/Hotelmenu-react-003/static/js/2.ce02c916.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hotelmenu-react-003/static/js/2.ce02c916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d4c0c51aec40d05301c",
+    "url": "/Hotelmenu-react-003/static/js/main.5ca74aa1.chunk.js"
+  },
+  {
+    "revision": "4480c0da28dc9642c804",
+    "url": "/Hotelmenu-react-003/static/js/runtime-main.2038e718.js"
+  }
+]);
